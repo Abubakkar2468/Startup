3e9851f3d1cc9baffae1e854d64e983eb76fac82
Abubakkar2468 <usmanabubakkar100@gmail.com>
2018-12-19 18:12:15 +0530

    implement lazyload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'feed', loadChildren: './feed-page/feed-page.module#FeedPageModule' },
+  {path: 'login', loadChildren: './login-page/login-page.module#LoginPageModule'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/feed-page/feed-page.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeedPageRoutingModule } from './feed-page-routing.module';
+import { FeedPageComponent } from './feed-page.component';
+
+@NgModule({
+  declarations: [
+    FeedPageComponent
+  ],
+  imports: [
+    CommonModule,
+    FeedPageRoutingModule
+  ]
+})
+export class FeedPageModule { }
